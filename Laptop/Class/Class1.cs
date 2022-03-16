a01bc5590263bdc68a9c3b1b5e6151544b218687
Fuad0010 <fuadft@code.edu.az>
2022-03-16 18:06:05 +0400

    Add project files.

---
+++ Laptop/Class/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laptop.Class
+{  
+   class laptop
+   {
+       public string brand;
+       public string videokarta;
+       public string processor;
+       public string ram;
+       public string hardDrive;
+       public double displaySize;
+   
+       public void FullName()
+       {
+           Console.WriteLine($"Brand: {brand}\n" +
+                             $"Graphics Coprocessor: {videokarta}\n" +
+                             $"Processor: {processor}\n" +
+                             $"RAM: {ram}\n" +
+                             $"Hard Drive: {hardDrive}\n" +
+                             $"Standing screen display size: {displaySize} Inches");
+       }
+   
+   
+   
+   }
+
+}
+
---
+++ Laptop/Program.cs
@@ -0,0 +1,27 @@
+﻿using Laptop.Class;
+using System;
+
+namespace Laptop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            laptop comp = new laptop();
+            {   
+                comp.brand = "Asus Rog Strix";
+                comp.videokarta = "RTX 2070";
+                comp.processor = "Core i7 - 9th";
+                comp.ram = "16 GB DDR4";
+                comp.hardDrive = "512 SSD";
+                comp.displaySize = 15.6;
+
+                comp.FullName();
+            }
+
+        }
+        
+   
+    
+    }
+}
